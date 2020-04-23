5e5f8bf9033e3d50030e69e3bb881c9a7adb42a2
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-04-24 01:30:31 +0200

    Add basic image controller

---
+++ FinalDemo/Controllers/ImageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FinalDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImageController : ControllerBase
+    {
+        public ImageController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<ImageDto> Get()
+        {
+            return new ImageDto[]
+            {
+            };
+        }
+    }
+}
