c5ac49f67a456085a9fe0e35d990c67961e6afdd
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-04-24 01:34:47 +0200

    Add ImageService and register it in Startup

--- FinalDemo/Startup.cs
+++ FinalDemo/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FinalDemo.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,8 @@ namespace FinalDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddTransient<ImageService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
