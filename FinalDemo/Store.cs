cf8575ede0fb364fadfa23a76e9d752531ef2fe0
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-04-24 01:47:27 +0200

    Create Store helper (and install newtonsoft.json)

---
+++ FinalDemo/Store.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace FinalDemo
+{
+    public static class Store
+    {
+        public static T ReadJsonFromFile<T>(string path)
+        {
+            try
+            {
+                var text = File.ReadAllText(path);
+                return JsonConvert.DeserializeObject<T>(text);
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+
+        public static void WriteJsonToFile<T>(string path, T data)
+        {
+            var text = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.WriteAllText(path, text);
+        }
+    }
+}
---
+++ FinalDemo/StorePaths.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalDemo
+{
+    public static class StorePaths
+    {
+        public const string Accounts = "data/accounts.json";
+        public const string Sessions = "data/sessions.json";
+        public const string Images = "data/images.json";
+        public static string Messages = "data/messages.json";
+    }
+}
