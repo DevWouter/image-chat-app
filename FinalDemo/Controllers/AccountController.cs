0758df3f5536ae50e9f365be736c2a73ff0be52e
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-04-24 07:22:05 +0200

    Add the creation of an account

---
+++ FinalDemo/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FinalDemo.Models;
+using FinalDemo.Services;
+
+namespace FinalDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly AccountService _accountService;
+
+        public AccountController(AccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public void PostAccount(AccountRequest body)
+        {
+            _accountService.Create(body.Username, body.Password);
+        }
+
+    }
+}
---
+++ FinalDemo/Models/AccountRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace FinalDemo.Models
+{
+    public class AccountRequest
+    {
+        public string Password { get; internal set; }
+        public string Username { get; internal set; }
+    }
+}
