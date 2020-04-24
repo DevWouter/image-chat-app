66da362e09df0e247592b9b9d9a5ac51e9361451
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-04-24 07:36:59 +0200

    Add ChatService

---
+++ FinalDemo/Services/ChatService.cs
@@ -0,0 +1,17 @@
+﻿using FinalDemo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalDemo.Services
+{
+    public class ChatService
+    {
+        public IEnumerable<Chat> GetForImageId(int imageId)
+        {
+            return Store.ReadJsonFromFile<IEnumerable<Chat>>(StorePaths.Messages)
+                .Where(x=> x.ImageId == imageId);
+        }
+    }
+}
--- FinalDemo/Startup.cs
+++ FinalDemo/Startup.cs
@@ -30,6 +30,7 @@ namespace FinalDemo
             services.AddTransient<ImageService>();
             services.AddTransient<AccountService>();
             services.AddTransient<SessionService>();
+            services.AddTransient<ChatService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
