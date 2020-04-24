ccdd3687e6833a8b13b8eea01f976690dac13eeb
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-04-24 07:15:34 +0200

    Add account service

---
+++ FinalDemo/Services/AccountService.cs
@@ -0,0 +1,35 @@
+﻿using FinalDemo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalDemo.Services
+{
+    public class AccountService
+    {
+        public Account Create(string username, string password)
+        {
+            var accounts = Store.ReadJsonFromFile<IEnumerable<Account>>(StorePaths.Accounts)
+                .ToList();
+
+            var nextId = accounts
+                .Select(x => x.Id)
+                .OrderByDescending(x => x)
+                .FirstOrDefault() + 1;
+
+            var newAccount = new Account
+            {
+                Id = nextId,
+                Name = username,
+                Password = password
+            };
+
+            accounts.Add(newAccount);
+
+            Store.WriteJsonToFile(StorePaths.Accounts, accounts);
+
+            return newAccount;
+        }
+    }
+}
--- FinalDemo/Startup.cs
+++ FinalDemo/Startup.cs
@@ -28,6 +28,7 @@ namespace FinalDemo
             services.AddControllers();
 
             services.AddTransient<ImageService>();
+            services.AddTransient<AccountService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
