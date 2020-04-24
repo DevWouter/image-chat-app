54fdc154226c5cdbb720bd7362957d7ecaae468f
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-04-24 07:24:56 +0200

    Add session class

---
+++ FinalDemo.Data/Session.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalDemo.Data
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public Guid SessionKey { get; set; }
+    }
+}
