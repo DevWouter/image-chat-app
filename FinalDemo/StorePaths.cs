2943a1c37a3d8ebc06b20b20d78cbbf900f1b629
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-04-24 08:25:16 +0200

    Make all storePaths a const

--- FinalDemo/StorePaths.cs
+++ FinalDemo/StorePaths.cs
@@ -5,6 +5,6 @@
         public const string Accounts = "data/accounts.json";
         public const string Sessions = "data/sessions.json";
         public const string Images = "data/images.json";
-        public static string Messages = "data/messages.json";
+        public const string Messages = "data/messages.json";
     }
 }
