2205fd79462355b3bc1aba030853459337ea9c75
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-04-24 07:46:16 +0200

    Add ChatService.Create

--- FinalDemo/Services/ChatService.cs
+++ FinalDemo/Services/ChatService.cs
@@ -13,5 +13,29 @@ namespace FinalDemo.Services
             return Store.ReadJsonFromFile<IEnumerable<Chat>>(StorePaths.Messages)
                 .Where(x=> x.ImageId == imageId);
         }
+
+        public Chat Create(int imageId, Account account, string message)
+        {
+            var chats = Store.ReadJsonFromFile<List<Chat>>(StorePaths.Messages);
+
+            var nextId = chats
+                .Select(x => x.Id)
+                .OrderByDescending(x => x)
+                .FirstOrDefault() + 1;
+
+            var newChat = new Chat
+            {
+                Id = nextId,
+                ImageId = imageId,
+                UserId = account.Id,
+                Content = message,
+            };
+
+            chats.Add(newChat);
+
+            Store.WriteJsonToFile(StorePaths.Messages, chats);
+
+            return newChat;
+        }
     }
 }
