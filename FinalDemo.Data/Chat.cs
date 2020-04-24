ffeb6b0b924146824f16b7c14cf34775859207ee
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-04-24 07:34:42 +0200

    Add Chat entity

---
+++ FinalDemo.Data/Chat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalDemo.Data
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public int ImageId { get; set; }
+        public int UserId { get; set; }
+        public string Content { get; set; }
+    }
+}
