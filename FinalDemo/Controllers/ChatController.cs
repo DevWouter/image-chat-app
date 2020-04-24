f87ba7799aac99e1e339f69f10d918b54512acc1
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-04-24 07:42:13 +0200

    Add ChatController with `GET api/chat/:imageId`

---
+++ FinalDemo/Controllers/ChatController.cs
@@ -0,0 +1,35 @@
+﻿using FinalDemo.Data;
+using FinalDemo.Models;
+using FinalDemo.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly ChatService _chatService;
+
+        public ChatController(ChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        [HttpGet]
+        [Route("{imageId}")]
+        public IEnumerable<ChatDto> GetMessageForImage(int imageId)
+        {
+            var allMessages = _chatService.GetForImageId(imageId);
+            return allMessages.Select(x => new ChatDto()
+            {
+                Message = x.Content,
+                Username = $"User {x.UserId}"
+            });
+        }
+    }
+}
---
+++ FinalDemo/Models/ChatDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalDemo.Models
+{
+    public class ChatDto
+    {
+        public string Username { get; set; }
+        public string Message { get; set; }
+    }
+}
