c5664b6a60c3c21461710138783527e519b03a8f
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-04-24 07:27:45 +0200

    Add SessionService (incl. SessionService.Create)

---
+++ FinalDemo/Services/SessionService.cs
@@ -0,0 +1,35 @@
+﻿using FinalDemo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalDemo.Services
+{
+    public class SessionService
+    {
+        public Session Create(int userId)
+        {
+            var sessions = Store.ReadJsonFromFile<IEnumerable<Session>>(StorePaths.Sessions)
+                .ToList();
+
+            var nextId = sessions
+                .Select(x => x.Id)
+                .OrderByDescending(x => x)
+                .FirstOrDefault() + 1;
+
+            var newSession = new Session
+            {
+                Id = nextId,
+                UserId = userId,
+                SessionKey= Guid.NewGuid()
+            };
+
+            sessions.Add(newSession);
+
+            Store.WriteJsonToFile(StorePaths.Sessions, sessions);
+
+            return newSession;
+        }
+    }
+}
--- FinalDemo/Startup.cs
+++ FinalDemo/Startup.cs
@@ -29,6 +29,7 @@ namespace FinalDemo
 
             services.AddTransient<ImageService>();
             services.AddTransient<AccountService>();
+            services.AddTransient<SessionService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
